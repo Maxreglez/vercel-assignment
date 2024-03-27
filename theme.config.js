faaf3d5f089705d529c74a82766806d3a187c759
Zelger, Max <m.zelger@sw-machines.de>
2024-03-27 14:07:11 +0100

    add questions

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{2024}</time> © Max Zelger.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
